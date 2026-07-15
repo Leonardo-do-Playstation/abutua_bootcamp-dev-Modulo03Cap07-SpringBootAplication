1829ab043ee48cb4cd6247e93c4f2a84cc92ded0
Leonardo do Playstation <leooliveiraboff@gmail.com>
2026-07-14 23:49:52 -0300

    refactor: add mapping annotation in ProductController class

--- src/main/java/com/abutua/productbackend/resources/ProductController.java
+++ src/main/java/com/abutua/productbackend/resources/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -20,6 +21,7 @@ import com.abutua.productbackend.services.ProductService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("products")
 public class ProductController {
 
     @Autowired
@@ -38,24 +40,24 @@ public class ProductController {
         return ResponseEntity.created(location).body(product);
     }
 
-    @GetMapping("products/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
         Product product = productService.getById(id);
         return ResponseEntity.ok(product);
     }
 
-    @GetMapping("products")  
+    @GetMapping  
     public List<Product> getProducts() { 
         return productService.getAll();
     }
 
-    @DeleteMapping("products/{id}")    
+    @DeleteMapping("{id}")    
     public ResponseEntity<Void> removeProduct(@PathVariable int id) {
         productService.deleteById(id);                
         return ResponseEntity.noContent().build(); 
     }
    
-    @PutMapping("products/{id}")    
+    @PutMapping("{id}")    
     public ResponseEntity<Void> updateProduct(@PathVariable int id, @RequestBody Product productUpdate) {
         productService.update(id, productUpdate);
         return ResponseEntity.ok().build(); 
