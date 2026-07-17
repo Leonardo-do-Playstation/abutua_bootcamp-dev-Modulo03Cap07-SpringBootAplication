d14702a10175f68f6d30256288003a066d2ecb94
Leonardo do Playstation <leooliveiraboff@gmail.com>
2026-07-16 21:58:52 -0300

    refact: ajusts get products endpoint

--- src/main/java/com/abutua/productbackend/resources/ProductController.java
+++ src/main/java/com/abutua/productbackend/resources/ProductController.java
@@ -47,8 +47,8 @@ public class ProductController {
     }
 
     @GetMapping  
-    public List<Product> getProducts() { 
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getProducts() { 
+        return ResponseEntity.ok(productService.getAll());
     }
 
     @DeleteMapping("{id}")    
