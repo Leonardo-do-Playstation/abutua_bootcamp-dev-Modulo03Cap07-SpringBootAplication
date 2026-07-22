dbe256d5c99b66989f1e0acae34fc841aae17f88
Leonardo do Playstation <leooliveiraboff@gmail.com>
2026-07-21 22:34:05 -0300

    refact: implements Column annotation in entity classes

--- src/main/java/com/abutua/productbackend/models/Category.java
+++ src/main/java/com/abutua/productbackend/models/Category.java
@@ -2,6 +2,7 @@ package com.abutua.productbackend.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Category implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+     @Column(nullable = false, unique = true)
     private String name;
 
     public Category(Long id, String name) {
--- src/main/java/com/abutua/productbackend/models/Product.java
+++ src/main/java/com/abutua/productbackend/models/Product.java
@@ -2,6 +2,7 @@ package com.abutua.productbackend.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +18,9 @@ public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(length = 1024)
     private String description;
 
     @ManyToOne
