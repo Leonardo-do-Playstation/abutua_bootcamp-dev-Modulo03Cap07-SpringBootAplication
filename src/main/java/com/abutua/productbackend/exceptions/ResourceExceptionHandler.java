f756cfa8c56ce1f964f388f54a712eb52be74ce5
Leonardo do Playstation <leooliveiraboff@gmail.com>
2026-07-27 18:59:00 -0300

    feat: implements StandartError class to improve HandlerException

--- src/main/java/com/abutua/productbackend/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/abutua/productbackend/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.abutua.productbackend.exceptions;
 
+import java.time.Instant;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -11,9 +13,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ResourceExceptionHandler{
     
+    
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> validationException(MethodArgumentNotValidException e, HttpServletRequest request){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation Error");
+    public ResponseEntity<StandartError> validationException(MethodArgumentNotValidException e, HttpServletRequest request){
+       
+        StandartError error = new StandartError();
+        error.setError("Validation Error");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        error.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        error.setTimeStamp(Instant.now());
+
+       
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 }
---
+++ src/main/java/com/abutua/productbackend/exceptions/StandartError.java
@@ -0,0 +1,47 @@
+package com.abutua.productbackend.exceptions;
+
+import java.time.Instant;
+
+public class StandartError {
+    
+    private Instant timeStamp;
+    private Integer statusCode;
+    private String error;
+    private String message;
+    private String path;
+    
+    public StandartError() {
+    }
+
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+    public String getError() {
+        return error;
+    }
+    public void setError(String error) {
+        this.error = error;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+}
